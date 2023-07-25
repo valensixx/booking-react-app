f0b55e5dc66bb63764af62e858d7845afc8858be
valensixx <valensixx@gmail.com>
2023-07-25 18:04:40 +0300

    header jsx and css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#8a2be2',
+      },
+    },
   },
   plugins: [],
 }
