f50c3f32a7c1e962e7b0fb6874f22b9128ad63d7
valensixx <valensixx@gmail.com>
2023-07-26 11:27:30 +0300

    added some code in RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
   return (
@@ -6,12 +7,13 @@ const RegisterPage = () => {
     <div className="mb-64">
         <h1 className="text-4xl text-center">Register</h1>
         <form className="max-w-md mx-auto mt-2">
-            <input type="email" placeholder="your@email.com" />
-            <input type="password" placeholder="password..." />
-            <button className="primary">Login</button>
+            <input type="text" placeholder="Enter your name..." />
+            <input type="email" placeholder="Enter your@email.com..." />
+            <input type="password" placeholder="Enter your password..." />
+            <button className="primary">Register</button>
             <div className="text-center text-gray-500 py-2">
                 <span>
-                    Do not have account? <Link className="underline text-black" to={'/register'}>Register now</Link>
+                    Allready have un account? <Link className="underline text-black" to={'/login'}>Login now</Link>
                 </span>
             </div>
         </form>
