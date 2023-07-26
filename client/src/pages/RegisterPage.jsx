0273f44d9834911271ecb8a750610f5100d13afc
valensixx <valensixx@gmail.com>
2023-07-26 14:32:46 +0300

    add funcionality in register page & install axios

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,14 +5,25 @@ const RegisterPage = () => {
   const [name,setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function registerUser(){
+    
+  } 
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
     <div className="mb-64">
         <h1 className="text-4xl text-center">Register</h1>
-        <form className="max-w-md mx-auto mt-2">
-            <input type="text" placeholder="Enter your name..." />
-            <input type="email" placeholder="Enter your@email.com..." />
-            <input type="password" placeholder="Enter your password..." />
+        <form className="max-w-md mx-auto mt-2" onSubmit={registerUser}>
+            <input type="text" placeholder="Enter your name..." 
+                value={name} onChange={e => setName(e.target.value)} 
+            />
+            <input type="email" placeholder="Enter your@email.com..." 
+                value={email} onChange={e => setEmail(e.target.value)}
+            />
+            <input type="password" placeholder="Enter your password..." 
+                value={password} onChange={e => setPassword(e.target.value)}
+            />
             <button className="primary">Register</button>
             <div className="text-center text-gray-500 py-2">
                 <span>
