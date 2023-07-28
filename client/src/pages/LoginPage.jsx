ffb4f9f238ab325909b4caa47bb6d64bbd687c89
valensixx <valensixx@gmail.com>
2023-07-28 14:44:25 +0300

    working on login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+async function handleLoginSubmit(e){
+    e.preventDefault();
+}
 
 const LoginPage = () => {
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center">Login</h1>
-                <form className="max-w-md mx-auto mt-2">
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password..." />
+                <form className="max-w-md mx-auto mt-2" onSubmit={handleLoginSubmit}>
+                    <input type="email"
+                        placeholder="your@email.com"
+                        value={email}
+                        onChange={e => setEmail(e.target, value)} />
+                    <input type="password"
+                        placeholder="password..."
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
                     <button className="primary">Login</button>
                     <div className="text-center text-gray-500 py-2">
                         <span>
