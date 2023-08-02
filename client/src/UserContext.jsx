811c4acd8b859b11e596cf5573e52386339a107d
valensixx <valensixx@gmail.com>
2023-08-02 15:54:11 +0300

    changes

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -7,11 +7,13 @@ export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
     useEffect(() => {
         if (!user) {
-            axios.get('/profile');
+            const {data} = axios.get('/profile').then(({ data }) => {
+                setUser(data);
+            });
         }
     }, [])
     return (
-        <UserContextProvider value={{ user, setUser }}>
+        <UserContextProvider value={{user, setUser}}>
             {children}
         </UserContextProvider>
 
